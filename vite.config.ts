6510764e7538e11eb192c8c183a15085fa6294da
Cuong Vuong Chi <cuongvuong.phoenix@gmail.com>
2022-03-21 15:41:26 +0700

    init: init *Vite* project & packages

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import * as path from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import icons from 'unplugin-icons/vite';
+import iconResolver from 'unplugin-icons/resolver';
+import components from 'unplugin-vue-components/vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src'),
+    },
+  },
+  plugins: [
+    vue({
+      reactivityTransform: true,
+    }),
+    icons({
+      scale: 1.5,
+    }),
+    components({
+      resolvers: [iconResolver()],
+      dts: 'src/types/components.d.ts',
+    }),
+  ],
+});
